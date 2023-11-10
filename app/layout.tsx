3ca1219caaceb3b438228f2f584e039f103b92b4
JM Lima <jml@jmlima.com>
2023-11-10 12:49:39 -0600

    downgrade next to 13.5

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,25 @@
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import './globals.css'
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'eggsly',
-  description: "no idea what eggsly is at this point, but you've got to have a meta description.",
-}
+  title: "eggsly",
+  description:
+    "no idea what eggsly is at this point, but you've got to have a meta description.",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`${inter.className} bg-blue-950 text-sky-50`}>
+        {children}
+      </body>
     </html>
-  )
+  );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <main className="bg-blue-950 text-sky-50">
+    <main className="">
       eggsly
     </main>
   )
