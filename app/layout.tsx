2d25b6548148371238f0e6d1ea13ed0efa84c1ac
JM Lima <jml@jmlima.com>
2023-11-10 12:01:42 -0600

    initial commit

---
+++ app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import './globals.css'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'eggsly',
+  description: "no idea what eggsly is at this point, but you've got to have a meta description.",
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  )
+}
