b8999bde4e5cb29dd6798aace04f35e44665f621
JM Lima <jml@jmlima.com>
2023-11-18 09:33:12 -0600

    added contact, about pages

---
+++ app/about/page.tsx
@@ -0,0 +1,10 @@
+export default function About() {
+    return (
+      <main>
+        {/*<main className="flex justify-center items-center min-h-screen">*/}
+        about page
+        {/* Other children elements can go here */}
+      </main>
+    );
+  }
+  
---
+++ app/contact/page.tsx
@@ -0,0 +1,10 @@
+export default function Contact() {
+    return (
+      <main>
+        {/*<main className="flex justify-center items-center min-h-screen">*/}
+        contact page
+        {/* Other children elements can go here */}
+      </main>
+    );
+  }
+  
